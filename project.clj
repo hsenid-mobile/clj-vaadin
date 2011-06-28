6e624b64a91cf5824d5b3df7734269c8eda9b9b6
hsanjeewa <hsanjeewa@gmail.com>
2011-06-28 16:45:12 -0700

    Edited project.clj via GitHub

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject clj-vaadin "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Clojure Wrapper for Vaadin"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]
                  ;[org.clojure/algo.monads "0.1.0-SNAPSHOT"]
                  [core.logic "0.6.1-SNAPSHOT"]
                  ;[org.clojure/java.data "0.0.1-SNAPSHOT"]
-                ; [hafni "1.0.5-SNAPSHOT"]
+                 ;[hafni "1.0.5-SNAPSHOT"]
                  [com.vaadin/vaadin "6.6.2"]
                  [org.eclipse.jetty/jetty-server "8.0.0.M3"]
                  [org.eclipse.jetty/jetty-webapp "8.0.0.M3"]]
